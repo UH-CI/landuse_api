0760aa3d9ea9852b7b944e007cda63bf81500d99
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-12-20 14:49:09 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+var hosts = ['https://localhost:9000','http://localhost:9000']
+var whiteList = ['testuser']
+
+var mongo_user ="";
+var mongo_pass ="";
+var mongo_host = "";
+var mongo_port = "";
+var mongo_database = "";
+var api_port = 8080;
+
+module.exports = { hosts: hosts, white_list: whiteList, mongo_user: mongo_user, mongo_pass: mongo_pass,mongo_host: mongo_host, mongo_port: mongo_port, mongo_database: mongo_database, api_port: api_port};
---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var configFile = require('./config.js');
+
+const mongo_user =configFile.mongo_user;
+const mongo_pass =configFile.mongo_pass;
+const mongo_host =configFile.mongo_host;
+const mongo_port =configFile.mongo_port;
+const mongo_database =configFile.mongo_database;
+mongoose.connect('mongodb://'+mongo_user+':'+mongo_pass+'@'+mongo_host+':'+mongo_port+'/'+mongo_database); // connect to our database
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = configFile.port;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
